fdacd2d7bfb1b620cad7f87f4a8f68caadd847b7
shirashko <shirashko@gmail.com>
2026-03-12 20:19:43 +0200

    train.sh: Update gpu type.

--- scripts/arithmetic/train.sh
+++ scripts/arithmetic/train.sh
@@ -6,7 +6,7 @@
 #SBATCH --error=logs/train_%j.err
 #SBATCH --time=24:00:00
 #SBATCH --partition=studentkillable
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:geforce_rtx_2080:1
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=64G
 
