9367bd95790dc0fa5439e037424601371dbfffcc
shirashko <shirashko@gmail.com>
2026-03-20 23:44:22 +0200

    download_gemma.py: Update to work with Pathlib library.

--- localized_undo/prepare_models/download_gemma.py
+++ localized_undo/prepare_models/download_gemma.py
@@ -13,7 +13,7 @@ from localized_undo.utils.paths import CACHE_DIR, WMDP_MODEL_DIR
 from localized_undo.utils.loss_functions import custom_makedirs, custom_login
 
 
-SAVE_DIR = WMDP_MODEL_DIR + "/gemma-2-2b"
+SAVE_DIR = WMDP_MODEL_DIR / "gemma-2-2b"
 
 def main():
     # 1) Log into HF
